96f46fd205b8f35b6a45e515741ae99b5e0958f2
muhammeta7 <muhammeta7@gmail.com>
2021-08-15 23:32:41 -0400

    get random coin from coin-gecko and retrieve ticker data

--- src/coinGecko/shared.js
+++ src/coinGecko/shared.js
@@ -2,8 +2,6 @@ const CoinGecko = require('coingecko-api');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const CoinGeckoClient = new CoinGecko();
 
-let coinList;
-
 const getCoinIds = () => {
     return CoinGeckoClient.coins.list();
 }
@@ -14,8 +12,9 @@ const csvWriter = (path, columnHeaders) => {
         header: columnHeaders
     });
     // Add path to save to local machine Update empty '<Your path/coingecko_api_practice/src/coinGecko/data> + nameOfFile.csv'
+    // <{id: 'jsonData properties...', title: 'Excel sheet column name'},{id: 'id2', title:'Column2'}>
+    // ===================================
     //     {id: id, title: 'Coin_ID'},
-    //     id: 'jsonData properties...', title: 'Excel sheet column name'
     //     {id: 'symbol', title: 'ticker'},
     //     {id: 'name', title: 'name'},
 }
@@ -28,13 +27,33 @@ const writeCsv = async () => {
     );
 }
 
+let coins = [];
 let coinId = "";
+
 const selectRandomCoinId = async () => {
-    let coins = await getCoinIds();
+    coins = await getCoinIds();
     coins = coins.data.slice(); // Creates copy of coin list array w/ length of 8779
     coinId = coins[Math.floor(Math.random() * coins.length)].id; // set random coin variable
+    console.log(coinId);
     return coinId;
 };
 
-selectRandomCoinId().then(() => console.log(coinId));
+const fetchCoin = () => {
+    return CoinGeckoClient.coins.fetchTickers(coinId);
+}
+
+let coinTickerData;
+const displayTickerFields = (someList) => {
+    let i = 0;
+    while( i < someList.length ){
+        console.log(someList[i]);
+        i++;
+    }
+}
+
+selectRandomCoinId().then( () => fetchCoin())
+    .then( resp => { coinTickerData = resp.data.tickers} )
+    .then( () => displayTickerFields(coinTickerData)
+);
+
 
