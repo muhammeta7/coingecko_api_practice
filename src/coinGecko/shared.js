fc1a627db694fe90194f63ebae64e7d121881de5
muhammeta7 <muhammeta7@gmail.com>
2021-08-15 21:37:27 -0400

    update instructions and clean up code

--- src/coinGecko/shared.js
+++ src/coinGecko/shared.js
@@ -1,30 +1,29 @@
-export const CoinGecko = require('coingecko-api');
-export const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-
+const CoinGecko = require('coingecko-api');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const CoinGeckoClient = new CoinGecko();
 
-export const getCoinList = async () => {
+
+export const getCoinIds = async () => {
     return CoinGeckoClient.coins.list();
 }
 
-export const csvWriter = (path, columnHeaders) => {
+const csvWriter = (path, columnHeaders) => {
     createCsvWriter({
         path: path,
         header: columnHeaders
-    }
-    // Add path to save to local machine Update empty ''
-    // header: [
+    });
+    // Add path to save to local machine Update empty '<Your path/coingecko_api_practice/src/coinGecko/data> + nameOfFile.csv'
     //     {id: id, title: 'Coin_ID'},
     //     id: 'jsonData properties...', title: 'Excel sheet column name'
     //     {id: 'symbol', title: 'ticker'},
     //     {id: 'name', title: 'name'},
-    // ]
-    )}
+}
 
-export const writeCsv = async () => {
-    getCoinList().then(
+const writeCsv = async () => {
+    getCoinIds().then(
         resp => {
             csvWriter.writeRecords(resp.data);
         }
     );
 }
+
